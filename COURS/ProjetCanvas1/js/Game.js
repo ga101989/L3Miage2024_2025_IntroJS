6144dcc078cd76e553ca35600c17bbda8fc6895a
Michel Buffa <michelbuffa@MacBook-Pro-de-Michel-2.local>
2025-01-28 17:39:25 +0100

    ajout écouteurs souris et collisions

--- COURS/ProjetCanvas1/js/Game.js
+++ COURS/ProjetCanvas1/js/Game.js
@@ -1,6 +1,7 @@
 import Player from "./Player.js";
 import Obstacle from "./Obstacle.js";
-
+import ObjetSouris from "./ObjetSouris.js";
+import { rectsOverlap } from "./collisions.js";
 import { initListeners } from "./ecouteurs.js";
 export default class Game {
     objetsGraphiques = [];
@@ -8,7 +9,10 @@ export default class Game {
     constructor(canvas) {
         this.canvas = canvas;
         // etat du clavier
-        this.inputStates = {};
+        this.inputStates = {
+            mouseX: 0,
+            mouseY: 0,
+        };
     }
 
     async init(canvas) {
@@ -17,14 +21,22 @@ export default class Game {
         this.player = new Player(100, 100);
         this.objetsGraphiques.push(this.player);
 
+        // Un objert qui suite la souris, juste pour tester
+        this.objetSouris = new ObjetSouris(200, 200, 25, 25, "orange");
+        this.objetsGraphiques.push(this.objetSouris);
+
+
         // On cree deux obstacles
-        let obstacle1 = new Obstacle(300, 300, 100, 100, "red");
+        let obstacle1 = new Obstacle(300, 0, 40, 600, "red");
         this.objetsGraphiques.push(obstacle1);
         let obstacle2 = new Obstacle(500, 500, 100, 100, "blue");
         this.objetsGraphiques.push(obstacle2);
 
+        // On ajoute la sortie
+        // TODO
+
         // On initialise les écouteurs de touches, souris, etc.
-        initListeners(this.inputStates);
+        initListeners(this.inputStates, this.canvas);
 
         console.log("Game initialisé");
     }
@@ -54,22 +66,111 @@ export default class Game {
     }
 
     drawAllObjects() {
+        // Dessine tous les objets du jeu
         this.objetsGraphiques.forEach(obj => {
             obj.draw(this.ctx);
         });
     }
 
     update() {
+        // Appelée par mainAnimationLoop
+        // donc tous les 1/60 de seconde
+        
+        // Déplacement du joueur. 
+        this.movePlayer();
+
+        // on met à jouer la position de objetSouris avec la position de la souris
+        // Pour un objet qui "suit" la souris mais avec un temps de retard, voir l'exemple
+        // du projet "charQuiTire" dans le dossier COURS
+        this.objetSouris.x = this.inputStates.mouseX;
+        this.objetSouris.y = this.inputStates.mouseY;
+
+        // On regarde si le joueur a atteint la sortie
+        // TODO
+
+    }
+
+    movePlayer() {
         this.player.vitesseX = 0;
+        this.player.vitesseY = 0;
         
         if(this.inputStates.ArrowRight) {
             this.player.vitesseX = 3;
         } 
-
         if(this.inputStates.ArrowLeft) {
             this.player.vitesseX = -3;
         } 
 
+        if(this.inputStates.ArrowUp) {
+            this.player.vitesseY = -3;
+        } 
+
+        if(this.inputStates.ArrowDown) {
+            this.player.vitesseY = 3;
+        } 
+
         this.player.move();
+
+        this.testCollisionsPlayer();
     }
+
+    testCollisionsPlayer() {
+        // Teste collision avec les bords du canvas
+        this.testCollisionPlayerBordsEcran();
+
+        // Teste collision avec les obstacles
+        this.testCollisionPlayerObstacles();
+       
+    }
+
+    testCollisionPlayerBordsEcran() {
+        // Raoppel : le x, y du joueur est en son centre, pas dans le coin en haut à gauche!
+        if(this.player.x - this.player.w/2 < 0) {
+            // On stoppe le joueur
+            this.player.vitesseX = 0;
+            // on le remet au point de contaxct
+            this.player.x = this.player.w/2;
+        }
+        if(this.player.x + this.player.w/2 > this.canvas.width) {
+            this.player.vitesseX = 0;
+            // on le remet au point de contact
+            this.player.x = this.canvas.width - this.player.w/2;
+        }
+
+        if(this.player.y - this.player.h/2 < 0) {
+            this.player.y = this.player.h/2;
+            this.player.vitesseY = 0;
+
+        }
+       
+        if(this.player.y + this.player.h/2 > this.canvas.height) {
+            this.player.vitesseY = 0;
+            this.player.y = this.canvas.height - this.player.h/2;
+        }
+    }
+
+    testCollisionPlayerObstacles() {
+        this.objetsGraphiques.forEach(obj => {
+            if(obj instanceof Obstacle) {
+                if(rectsOverlap(this.player.x-this.player.w/2, this.player.y - this.player.h/2, this.player.w, this.player.h, obj.x, obj.y, obj.w, obj.h)) {
+                    // collision
+
+                    // ICI TEST BASIQUE QUI ARRETE LE JOUEUR EN CAS DE COLLIION.
+                    // SI ON VOULAIT FAIRE MIEUX, ON POURRAIT PAR EXEMPLE REGARDER OU EST LE JOUEUR
+                    // PAR RAPPORT A L'obstacle courant : il est à droite si son x est plus grand que le x de l'obstacle + la largeur de l'obstacle
+                    // il est à gauche si son x + sa largeur est plus petit que le x de l'obstacle
+                    // etc.
+                    // Dans ce cas on pourrait savoir comment le joueur est entré en collision avec l'obstacle et réagir en conséquence
+                    // par exemple en le repoussant dans la direction opposée à celle de l'obstacle...
+                    // Là par défaut on le renvoie en x=10 y=10 et on l'arrête
+                    console.log("Collision avec obstacle");
+                    this.player.x = 10;
+                    this.player.y = 10;
+                    this.player.vitesseX = 0;
+                    this.player.vitesseY = 0;
+                }
+            }
+        });
+    }
+
 }
--- COURS/ProjetCanvas1/js/ecouteurs.js
+++ COURS/ProjetCanvas1/js/ecouteurs.js
@@ -1,4 +1,4 @@
-function initListeners(inputStates) {
+function initListeners(inputStates, canvas) {
     window.onkeydown = (event) => {
         console.log("Touche pressée : " + event.key);
         if(event.key === "ArrowRight") {
@@ -6,6 +6,12 @@ function initListeners(inputStates) {
         }
         if(event.key === "ArrowLeft") {
             inputStates.ArrowLeft = true;
+        }
+        if(event.key === "ArrowUp") {
+            inputStates.ArrowUp = true;
+        }
+        if(event.key === "ArrowDown") {
+            inputStates.ArrowDown = true;
         }
     }
 
@@ -17,6 +23,18 @@ function initListeners(inputStates) {
         if(event.key === "ArrowLeft") {
             inputStates.ArrowLeft = false;
         }
+        if(event.key === "ArrowUp") {
+            inputStates.ArrowUp = false;
+        }
+        if(event.key === "ArrowDown") {
+            inputStates.ArrowDown = false;
+        }
+    }
+
+    window.onmousemove = (event) => {
+        // get proper x and y for the mouse in the canvas
+        inputStates.mouseX = event.clientX - canvas.getBoundingClientRect().left;
+        inputStates.mouseY = event.clientY - canvas.getBoundingClientRect().top;
     }
 }
 
